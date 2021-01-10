a5b6a0cd1b72c8c858fcb707fe7ed093f4afd4e6
Sarah Chun <66026667+sarahschun@users.noreply.github.com>
2021-01-09 20:37:50 -0500

    Update content.js

--- content.js
+++ content.js
@@ -1,56 +1,48 @@
+function splitString(){
 
 
-function readText() {
+}
+
+
+
+function replaceText() {
+
 	var x = document.getElementById("");
 	var word = x.elements[0].value;
 	var wordLength = word.length;
 
 //I'm sorry
 
-	if (word = "sorry"){
+	if (word.equals("sorry"){
 		word = "thank you";
     console.log(word);
-	
-  } else {
-		word = "sorry";
-	}
+  } 
 	
 //I think
-	
-	if (word = "I think"){
+	if (word.equals("I think")){
 		word = "It'd";
     cosole.log(word);
-	} else {
-		word = "I think";
-	}
-	
+	} 
   //guys -> everyone or y'all
 	
-	if (word = "guys"){
+	if (word.equals("guys")){
 		word = "everyone";
     cosole.log(word);
 	} 
-  else {
-		word = "guys";
-	}
-
+  
 	//sorry
 	
-	if (word = "sorry"){
+	if (word.equals("sorry")){
 		word = "nice catch ";
     cosole.log(word);
-	} else {
-		word = "sorry";
 	}
 
   //just
-  if (word = "just"){
+  if (word.equals("just")){
     word = " ";
   console.log(word);
   }
-  else{
-    word = "just";
-  }
+  
   
   document.getElementById("estimate").innerHTML = "Total cost of your sign: <br> £" + totalCorrect;
 	document.getElementById("prettySign").innerHTML = sign;
